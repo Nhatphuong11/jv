000da35415732761606561e97671be215baeae8e
Nhatphuong11 <phuong201101@gmail.com>
2023-09-18 17:30:33 +0700

    ss2

--- src/Th/b4.java
+++ src/Th/b4.java
@@ -21,7 +21,6 @@ public class b4 {
         if (n < 2) {
             return false;
         }
-        // check so nguyen to khi n >= 2
         int squareRoot = (int) Math.sqrt(n);
         for (int i = 2; i <= squareRoot; i++) {
             if (n % i == 0) {
